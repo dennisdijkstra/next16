b2f65bc3e0aa30acab0a50b28cc244e823b62831
Dennis Dijkstra <mail@dennisdijkstra.com>
2026-01-30 15:53:54 +0100

    Remove using state change within useEffect

--- client/src/app/components/Modal.tsx
+++ client/src/app/components/Modal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode, useState, useEffect, useRef, MouseEvent } from 'react'
+import { ReactNode, useEffect, useRef, MouseEvent } from 'react'
 import { createPortal } from 'react-dom'
 import { X } from '@phosphor-icons/react'
 import Button from '@/components/Button'
@@ -21,7 +21,6 @@ export default function Modal({
 }: ModalProps) {
   const overlayRef = useRef<HTMLDivElement>(null)
   const modalRef = useRef<HTMLDivElement>(null)
-  const [isMounted, setIsMounted] = useState(false)
 
   const animate = (isOpen: boolean) => {
     if (overlayRef.current) {
@@ -43,31 +42,36 @@ export default function Modal({
   }
 
   useEffect(() => {
-    setIsMounted(true)
     setTimeout(() => animate(true), 0)
   }, [])
 
+  const modalRoot = typeof document !== 'undefined'
+    ? document.getElementById('modal-root')
+    : null
+
+  if (!modalRoot) {
+    return null
+  }
+
   return (
-    isMounted ? (
-      createPortal(
-        <>
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10" ref={overlayRef} onClick={onClose} />
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className="relative w-150 h-100 z-20" ref={modalRef}>
-              <div className="w-full h-full px-10 pt-10 pb-27 rounded-md bg-white">
-                <button className="absolute top-5 right-5" onClick={onClose}>
-                  <X size={24} weight="bold" />
-                </button>
-                {title && <h1 className="text-3xl text-center font-bold">{title}</h1>}
-                <div className="mt-10">{children}</div>
-                <div className="absolute left-0 bottom-0 w-full flex">
-                  <Button type='submit' className='w-full rounded-none' onClick={onConfirm}>Ok</Button>
-                  <Button type='submit' className='w-full rounded-none' onClick={onClose}>Cancel</Button>
-                </div>
+    createPortal(
+      <>
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-10" ref={overlayRef} onClick={onClose} />
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="relative w-150 h-100 z-20" ref={modalRef}>
+            <div className="w-full h-full px-10 pt-10 pb-27 rounded-md bg-white">
+              <button className="absolute top-5 right-5" onClick={onClose}>
+                <X size={24} weight="bold" />
+              </button>
+              {title && <h1 className="text-3xl text-center font-bold">{title}</h1>}
+              <div className="mt-10">{children}</div>
+              <div className="absolute left-0 bottom-0 w-full flex">
+                <Button type='submit' className='w-full rounded-none' onClick={onConfirm}>Ok</Button>
+                <Button type='submit' className='w-full rounded-none' onClick={onClose}>Cancel</Button>
               </div>
             </div>
           </div>
-        </>, document.getElementById('modal-root')!)
-    ) : null
+        </div>
+      </>, modalRoot)
   )
 }
